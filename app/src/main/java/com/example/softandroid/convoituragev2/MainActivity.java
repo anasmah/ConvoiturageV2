3e112d114364b2e19aa59598363d871eaf2c8db2
habib94 <elhabib.benahmed@gmail.com>
2016-12-04 22:02:54 +0000

    cmooit 22

--- app/src/main/java/com/example/softandroid/convoituragev2/MainActivity.java
+++ app/src/main/java/com/example/softandroid/convoituragev2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("ddd","dd");
+        Log.i("dd","d");
     }
 }
